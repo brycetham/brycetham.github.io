81d967e54f937cd79662d0964d4692c19186d015
Bryce Tham <brycetham@gmail.com>
2019-08-26 18:01:52 -0700

    Started at Cisco

--- js/brycetham.js
+++ js/brycetham.js
@@ -6,4 +6,15 @@
 
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 400, function(){
+          window.location.hash = hash;
+        });
+      }
+    });
 });
