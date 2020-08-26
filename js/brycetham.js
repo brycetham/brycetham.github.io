a3bd0bbde821bdca67a59f9cb42081262418d697
Bryce Tham <brycetham@gmail.com>
2020-08-26 00:24:23 -0700

    Cisco 1 Year Update

--- js/brycetham.js
+++ js/brycetham.js
@@ -17,4 +17,16 @@ $(document).ready(function(){
         });
       }
     });
+
+    VANTA.NET({
+      el: "body",
+      mouseControls: true,
+      touchControls: true,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      color: 0xe1e1e1,
+      backgroundColor: 0xffffff
+    });
 });
